16978b9470680e288ce8abc88d379bbd6601f78a
OsmanFurkanNalbant <osmanfnalbant@outlook.com>
2022-09-14 21:57:53 +0300

    upddel

---
+++ sql.sql
@@ -0,0 +1,105 @@
+create database test;
+
+create table deneme (
+    id int,
+    name varchar(50),
+    birthdayDate int,
+    email varchar(50),
+);
+
+insert into deneme (id, name, email, birthdayDate) values (1, 'Dyann', 'dhoundesome0@archive.org', 2000);
+insert into deneme (id, name, email, birthdayDate) values (2, 'Waiter', 'wtrotton1@ibm.com', 2010);
+insert into deneme (id, name, email, birthdayDate) values (3, 'Tonnie', 'tclayill2@yahoo.co.jp', 1996);
+insert into deneme (id, name, email, birthdayDate) values (4, 'Edwin', 'emcrobb3@microsoft.com', 2002);
+insert into deneme (id, name, email, birthdayDate) values (5, 'Lolita', 'lellingworth4@jiathis.com', 2011);
+insert into deneme (id, name, email, birthdayDate) values (6, 'Hettie', 'hmaasze5@google.co.uk', 1999);
+insert into deneme (id, name, email, birthdayDate) values (7, 'Jonathan', 'jsichardt6@webmd.com', 1996);
+insert into deneme (id, name, email, birthdayDate) values (8, 'Chaddie', 'cgoodlet7@samsung.com', 2008);
+insert into deneme (id, name, email, birthdayDate) values (9, 'Wendel', 'wcloss8@arizona.edu', 1992);
+insert into deneme (id, name, email, birthdayDate) values (10, 'Peyton', 'pdurie9@hhs.gov', 1999);
+insert into deneme (id, name, email, birthdayDate) values (11, 'Porty', 'polennikova@illinois.edu', 2004);
+insert into deneme (id, name, email, birthdayDate) values (12, 'Corette', 'clemmonb@unicef.org', 1994);
+insert into deneme (id, name, email, birthdayDate) values (13, 'Timmie', 'tnicholesc@sitemeter.com', 1999);
+insert into deneme (id, name, email, birthdayDate) values (14, 'Barbie', 'bhamped@statcounter.com', 2011);
+insert into deneme (id, name, email, birthdayDate) values (15, 'Pacorro', 'pparadisee@economist.com', 1970);
+insert into deneme (id, name, email, birthdayDate) values (16, 'Ginger', 'gfitterf@stanford.edu', 2002);
+insert into deneme (id, name, email, birthdayDate) values (17, 'Niall', 'nafieldg@abc.net.au', 2006);
+insert into deneme (id, name, email, birthdayDate) values (18, 'Mordy', 'mcalcotth@hostgator.com', 1995);
+insert into deneme (id, name, email, birthdayDate) values (19, 'Tammara', 'tgateshilli@topsy.com', 1999);
+insert into deneme (id, name, email, birthdayDate) values (20, 'Mack', 'mjeacockj@gizmodo.com', 2002);
+insert into deneme (id, name, email, birthdayDate) values (21, 'Sibilla', 'stybaltk@google.co.jp', 2006);
+insert into deneme (id, name, email, birthdayDate) values (22, 'Kris', 'kdarbonl@yellowpages.com', 1991);
+insert into deneme (id, name, email, birthdayDate) values (23, 'Rex', 'rdunnem@illinois.edu', 2009);
+insert into deneme (id, name, email, birthdayDate) values (24, 'Dorena', 'dmcgaugeyn@live.com', 1991);
+insert into deneme (id, name, email, birthdayDate) values (25, 'Jillane', 'jpenvardeno@furl.net', 1994);
+insert into deneme (id, name, email, birthdayDate) values (26, 'Jarrett', 'jmionp@ifeng.com', 1970);
+insert into deneme (id, name, email, birthdayDate) values (27, 'Nancee', 'nberthelmotq@nyu.edu', 2000);
+insert into deneme (id, name, email, birthdayDate) values (28, 'Lorene', 'lbetchleyr@google.com.br', 2002);
+insert into deneme (id, name, email, birthdayDate) values (29, 'Ray', 'rdonnells@ustream.tv', 2009);
+insert into deneme (id, name, email, birthdayDate) values (30, 'Elyse', 'eabendrotht@comsenz.com', 2000);
+insert into deneme (id, name, email, birthdayDate) values (31, 'Laurice', 'lrammellu@macromedia.com', 1993);
+insert into deneme (id, name, email, birthdayDate) values (32, 'Kameko', 'kalbersv@nps.gov', 2012);
+insert into deneme (id, name, email, birthdayDate) values (33, 'Kala', 'kmccarlichw@usgs.gov', 1984);
+insert into deneme (id, name, email, birthdayDate) values (34, 'Michael', 'mgennx@who.int', 2010);
+insert into deneme (id, name, email, birthdayDate) values (35, 'Ina', 'iranniey@about.me', 2012);
+insert into deneme (id, name, email, birthdayDate) values (36, 'Westbrooke', 'wcranstonez@wikia.com', 2006);
+insert into deneme (id, name, email, birthdayDate) values (37, 'Wylma', 'wgiovannazzi10@nih.gov', 1996);
+insert into deneme (id, name, email, birthdayDate) values (38, 'Maurie', 'mkinchington11@jalbum.net', 1967);
+insert into deneme (id, name, email, birthdayDate) values (39, 'Kitty', 'kdogg12@dyndns.org', 2003);
+insert into deneme (id, name, email, birthdayDate) values (40, 'Magnum', 'mgreenley13@dedecms.com', 2008);
+insert into deneme (id, name, email, birthdayDate) values (41, 'Phillida', 'pjefferies14@altervista.org', 2005);
+insert into deneme (id, name, email, birthdayDate) values (42, 'Moira', 'msumner15@nhs.uk', 2002);
+insert into deneme (id, name, email, birthdayDate) values (43, 'Demott', 'dbuyers16@netlog.com', 2011);
+insert into deneme (id, name, email, birthdayDate) values (44, 'Rodie', 'rhartless17@pbs.org', 1988);
+insert into deneme (id, name, email, birthdayDate) values (45, 'Helga', 'hdutson18@wikia.com', 2007);
+insert into deneme (id, name, email, birthdayDate) values (46, 'Elwood', 'eheinke19@china.com.cn', 2010);
+insert into deneme (id, name, email, birthdayDate) values (47, 'Ingrid', 'ikippen1a@hugedomains.com', 1995);
+insert into deneme (id, name, email, birthdayDate) values (48, 'Valeda', 'vbarnham1b@bravesites.com', 2009);
+insert into deneme (id, name, email, birthdayDate) values (49, 'Barny', 'bdonne1c@livejournal.com', 2003);
+insert into deneme (id, name, email, birthdayDate) values (50, 'Patti', 'pdrennan1d@seesaa.net', 2006);
+
+
+UPDATE deneme
+SET name='osmanfnalbant',
+    email='osmanfnalbant@gmail.com',
+    birthdayDATE=2001
+WHERE id=1;
+
+UPDATE deneme
+SET name='messi',
+    email='messi@gmail.com',
+    birthdayDATE=1998
+WHERE id=2;
+
+UPDATE deneme
+SET name='kawami',
+    email='kawami@gmail.com',
+    birthdayDATE=2005
+WHERE id=3;
+
+UPDATE deneme
+SET name='kiryu',
+    email='uyrik@gmail.com',
+    birthdayDATE=2001
+WHERE id=4;
+
+UPDATE deneme
+SET name='merdark',
+    email='darkmer@gmail.com',
+    birthdayDATE=2001
+WHERE id=5;
+
+DELETE FROM deneme
+WHERE id=1;
+
+DELETE FROM deneme
+WHERE id=2;
+
+DELETE FROM deneme
+WHERE id=3;
+
+DELETE FROM deneme
+WHERE id=4;
+
+DELETE FROM deneme
+WHERE id=5;
